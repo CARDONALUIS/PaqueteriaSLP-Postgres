a9035a90e9de02be0449968fb0436e955ca898a6
CARDONALUIS <40583861+CARDONALUIS@users.noreply.github.com>
2021-05-15 23:02:28 -0500

    Tabla HorarioPersonal
    Tabla HorarioPersonal

--- src/postgresql_paqueteriaslp/Views/HorarioPersonal.java
+++ src/postgresql_paqueteriaslp/Views/HorarioPersonal.java
@@ -16,14 +16,14 @@ public class HorarioPersonal extends javax.swing.JFrame {
     String pass="postgres";
     String user ="postgres";
     DefaultTableModel modelo = new DefaultTableModel();
-    int idSucursalAct;
+    int idPersonal;
     
     public HorarioPersonal() {
         initComponents();
         estableceConexion();
         modelo_tabla();
         fillTabla();
-        limpiaControles();
+        limpiaControlesYAddLlaves();
     }
     
     public void estableceConexion()
@@ -35,7 +35,7 @@ public class HorarioPersonal extends javax.swing.JFrame {
         }
     }
     
-    public void limpiaControles()
+    public void limpiaControlesYAddLlaves()
     {
         JCBLunes.setSelected(false);
         JCBMartes.setSelected(false);
@@ -44,8 +44,31 @@ public class HorarioPersonal extends javax.swing.JFrame {
         JCBViernes.setSelected(false);
         JCBSabado.setSelected(false);
         JCBDomingo.setSelected(false);
-        JTHoras.setText("");
-         
+        agregaComboBox();
+        JTHoras.setText("");         
+    }
+    
+    public void agregaComboBox()
+    {
+        String datos[] = new String[3];
+        JCBPersonal.removeAllItems();
+                
+        try {
+            Statement at = conexion.createStatement();
+            ResultSet rs = at.executeQuery("SELECT * FROM paqueteria.personal");
+            while(rs.next())
+            {
+                datos[0] = rs.getString("nss");
+                datos[1] = rs.getString("nombre");
+                
+                JCBPersonal.addItem(datos[0]+ "-" +datos[1]);
+            }
+            
+            rs.close();
+            at.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No pudimos agregar los items");
+        }
     }
     
     public void modelo_tabla()
@@ -78,6 +101,122 @@ public class HorarioPersonal extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "No pudimos actualizar tu tabla");
         }
     }
+    
+    public String obtenDias()
+    {
+        String horarios = "";
+        
+        if(JCBLunes.isSelected())
+        {
+            horarios += "Lunes,";
+        }
+        
+        if (JCBMartes.isSelected())
+        {
+            horarios += "Martes,";
+        }
+
+        if (JCBMiercoles.isSelected())
+        {
+            horarios += "Miercoles,";
+        }
+
+        if (JCBJueves.isSelected())
+        {
+            horarios += "Jueves,";
+        }
+
+        if (JCBViernes.isSelected())
+        {
+            horarios += "Viernes,";
+        }
+
+        if (JCBSabado.isSelected())
+        {
+            horarios += "Sabado,";
+        }
+
+        if (JCBDomingo.isSelected())
+        {
+            horarios += "Domingo,";
+        }
+
+            return horarios;
+    }
+    
+    public void ChequeaBoxHorario(String _Horari)
+    {
+        int i = 0;
+        String cadTem = "";
+
+        while (i < _Horari.length())
+        {
+            cadTem += _Horari.charAt(i);
+
+            switch (cadTem)
+            {
+                case "Lu":
+                    JCBLunes.setSelected(true);
+                    cadTem = "";
+                    i += 4;
+                    break;
+                case "Ma":
+                    JCBMartes.setSelected(true);
+                    cadTem = "";
+                    i += 5;
+                    break;
+                case "Mi":
+                    JCBMiercoles.setSelected(true);
+                    cadTem = "";
+                    i += 8;
+                    break;
+                case "Ju":
+                    JCBJueves.setSelected(true);
+                    cadTem = "";
+                    i += 5;
+                    break;
+                case "Vi":
+                    JCBViernes.setSelected(true);
+                    cadTem = "";
+                    i += 6;
+                    break;
+                case "Sa":
+                    JCBSabado.setSelected(true);
+                    cadTem = "";
+                    i += 5;
+                    break;
+                case "Do":
+                    JCBDomingo.setSelected(true);
+                    cadTem = "";
+                    i += 6;
+                    break;
+
+            }
+            i++;
+        }
+    }
+    
+    public void obtenIDComboBox()
+    {  
+        String it = (String)JCBPersonal.getSelectedItem();
+        
+        try {
+            Statement at = conexion.createStatement();
+            ResultSet rs = at.executeQuery("SELECT * FROM paqueteria.personal WHERE nss = '"+it.split("-")[0]+"'");
+            while(rs.next())
+            {
+                idPersonal = Integer.parseInt(rs.getString("idpersonal"));
+            }
+            
+            rs.close();
+            at.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No pudimos econtrar la paersona a agregar");
+        }
+        
+
+    }
+        
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -105,7 +244,12 @@ public class HorarioPersonal extends javax.swing.JFrame {
         JBActualizar = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
 
         JTHorarioPersonal.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -118,6 +262,11 @@ public class HorarioPersonal extends javax.swing.JFrame {
 
             }
         ));
+        JTHorarioPersonal.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                JTHorarioPersonalMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(JTHorarioPersonal);
 
         JCBPersonal.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
@@ -141,10 +290,25 @@ public class HorarioPersonal extends javax.swing.JFrame {
         jLabel2.setText("Horas:");
 
         JBInsertar.setText("Insertar");
+        JBInsertar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBInsertarActionPerformed(evt);
+            }
+        });
 
         JTEliminar.setText("Eliminar");
+        JTEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JTEliminarActionPerformed(evt);
+            }
+        });
 
         JBActualizar.setText("Actualizar");
+        JBActualizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBActualizarActionPerformed(evt);
+            }
+        });
 
         jLabel3.setText("Personal:");
 
@@ -156,42 +320,45 @@ public class HorarioPersonal extends javax.swing.JFrame {
                 .addGap(23, 23, 23)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(JTHoras, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(JCBLunes)
+                                .addComponent(jLabel2)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(JCBMartes)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(JCBMiercoles)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(JCBJueves))
+                                .addComponent(JTHoras, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addComponent(jLabel3)
-                                .addGap(18, 18, 18)
-                                .addComponent(JCBPersonal, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(JBInsertar)
-                                .addGap(18, 18, 18)
-                                .addComponent(JTEliminar))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(JBActualizar)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(JCBViernes)
+                                        .addComponent(JCBLunes)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(JCBMartes)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(JCBMiercoles)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(JCBJueves)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                        .addComponent(JCBSabado)))
+                                        .addComponent(JCBViernes))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(10, 10, 10)
+                                        .addComponent(jLabel3)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(JCBPersonal, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(JCBDomingo))))
-                    .addComponent(jLabel1))
-                .addContainerGap(39, Short.MAX_VALUE))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(JBInsertar)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(JTEliminar))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(JCBSabado)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(JCBDomingo))))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 685, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(31, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(JBActualizar)
+                        .addGap(288, 288, 288))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -229,6 +396,83 @@ public class HorarioPersonal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        // TODO add your handling code here:
+        Home vH = new Home();
+        vH.setVisible(true);
+        this.dispose();     
+    }//GEN-LAST:event_formWindowClosing
+
+    private void JBInsertarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBInsertarActionPerformed
+        try {
+            
+            String cadDias = obtenDias();
+            Statement st = conexion.createStatement();
+            obtenIDComboBox();
+            String sql = "INSERT INTO paqueteria.horariopersonal(idPersonal,horario, horas)"
+                    +"VALUES("+idPersonal+",'"+cadDias+"','"+JTHoras.getText()+"')";
+            
+            st.executeUpdate(sql);
+            st.close();
+            fillTabla();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al insertar "+e);
+        }
+        limpiaControlesYAddLlaves();
+    }//GEN-LAST:event_JBInsertarActionPerformed
+
+    private void JTEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTEliminarActionPerformed
+        // TODO add your handling code here:
+        JCBPersonal.setEnabled(true);
+        try {
+            Statement st = conexion.createStatement();
+            String sql = "DELETE FROM paqueteria.horariopersonal WHERE idpersonal = "+idPersonal+"";                              
+            st.executeUpdate(sql);
+            st.close();
+            fillTabla();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al eliminar "+e);
+        }
+        
+        limpiaControlesYAddLlaves();
+    }//GEN-LAST:event_JTEliminarActionPerformed
+
+    private void JBActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBActualizarActionPerformed
+        JCBPersonal.setEnabled(true);
+        try {
+            Statement st = conexion.createStatement();
+            String sql = "UPDATE paqueteria.horariopersonal SET horario = '"+obtenDias()+"', horas"
+                    + " = '"+JTHoras.getText()+"' WHERE idpersonal = "+idPersonal+" ";
+            st.executeUpdate(sql);
+            st.close();
+            fillTabla();
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al actualizar "+e);
+        }
+        
+        limpiaControlesYAddLlaves();
+    }//GEN-LAST:event_JBActualizarActionPerformed
+
+    private void JTHorarioPersonalMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JTHorarioPersonalMouseClicked
+        // TODO add your handling code here:
+        limpiaControlesYAddLlaves();
+        JCBPersonal.setEnabled(false);
+        
+        int seleccionar = JTHorarioPersonal.rowAtPoint(evt.getPoint());
+        
+        idPersonal = Integer.parseInt(String.valueOf(JTHorarioPersonal.getValueAt(seleccionar, 0)));
+        
+        JTHoras.setText(String.valueOf(JTHorarioPersonal.getValueAt(seleccionar, 1)));
+        
+        String cadDias = String.valueOf(JTHorarioPersonal.getValueAt(seleccionar, 2));
+        
+        ChequeaBoxHorario(cadDias);
+                      
+    }//GEN-LAST:event_JTHorarioPersonalMouseClicked
 
     /**
      * @param args the command line arguments
