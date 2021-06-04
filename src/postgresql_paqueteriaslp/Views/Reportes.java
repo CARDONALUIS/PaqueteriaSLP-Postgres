215ae95a1751d3acba91b228cf91d69f3f213abd
CARDONALUIS <40583861+CARDONALUIS@users.noreply.github.com>
2021-06-03 21:50:03 -0500

    Primera parte reporte consulta 1
    Primera parte reporte consulta 1

--- src/postgresql_paqueteriaslp/Views/Reportes.java
+++ src/postgresql_paqueteriaslp/Views/Reportes.java
@@ -5,6 +5,14 @@
  */
 package postgresql_paqueteriaslp.Views;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import postgresql_paqueteriaslp.Home;
 
 /**
@@ -12,14 +20,81 @@ import postgresql_paqueteriaslp.Home;
  * @author Luis Pablo Rocha
  */
 public class Reportes extends javax.swing.JFrame {
-
+    String pass = "postgres";
+    String user = "postgres";
+    Connection conexion;
+    DefaultTableModel modelo = new DefaultTableModel();
     /**
      * Creates new form Reportes
      */
     public Reportes() {
         initComponents();
+        estableceConexion();
+        //modelo_tabla();
+        //modelo_tabla_Arrendado();
+        rellenaCombos();
     }
+    
+    public void estableceConexion()
+    {
+        try {
+            conexion = DriverManager.getConnection("jdbc:postgresql://localhost:5432/paqueteriaSLP",user, pass);            
+        }catch(SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al establecer conexion!!!"+ ex);
+        }
+    }
+    
+    public void rellenaCombos()
+    {
+        String datos[] = new String[3];
+        JCBSucursal.removeAllItems();
+        JCBTipo.removeAllItems();
+        
+        JCBTipo.addItem("Comprado");
+        JCBTipo.addItem("Arrendado");
+                
+        try {
+            Statement at = conexion.createStatement();
+            ResultSet rs = at.executeQuery("SELECT * FROM paqueteria.sucursal");
+            while(rs.next())
+            {
+                datos[0] = rs.getString("idsucursal");
+                datos[1] = rs.getString("nombresucursal");
 
+                JCBSucursal.addItem(datos[0]+ "-" +datos[1]);
+            }           
+            rs.close();
+            at.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No pudimos agregar los items");
+        }
+    }
+    
+    
+    public void modelo_tabla_Arrendado()
+    {
+        //JTCamiones.setAutoCreateColumnsFromModel(false);
+        
+        modelo.setColumnCount(0);
+        
+        modelo.addColumn("Tiempo de Contrato");
+        modelo.addColumn("Cantidad de Camiones");
+       
+        JTCamiones.setModel(modelo);
+    }
+    
+    public void modelo_tabla_Comprado()
+    {
+        modelo.setColumnCount(0);
+                
+        modelo.addColumn("Cantidad");
+        modelo.addColumn("año de compra");
+       
+        JTCamiones.setModel(modelo);
+    }
+    
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -29,6 +104,19 @@ public class Reportes extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        JCBSucursal = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        JCBTipo = new javax.swing.JComboBox<>();
+        JBEjecutaC1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        JTCamiones = new javax.swing.JTable();
+        jLabel4 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        JBEjecutaC2 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
@@ -36,15 +124,112 @@ public class Reportes extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setText("CONSULTA 1 CAMIONES");
+
+        JCBSucursal.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jLabel2.setText("Sucursal");
+
+        jLabel3.setText("TipoCamion");
+
+        JCBTipo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        JBEjecutaC1.setText("Ejecutar");
+        JBEjecutaC1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBEjecutaC1ActionPerformed(evt);
+            }
+        });
+
+        JTCamiones.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane1.setViewportView(JTCamiones);
+
+        jLabel4.setText("CONSULTA 2");
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {},
+                {},
+                {},
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
+
+        JBEjecutaC2.setText("Ejecutar");
+        JBEjecutaC2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBEjecutaC2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 580, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 489, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jLabel1)
+                                .addGroup(layout.createSequentialGroup()
+                                    .addGap(224, 224, 224)
+                                    .addComponent(JBEjecutaC1))
+                                .addGroup(layout.createSequentialGroup()
+                                    .addComponent(jLabel2)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 479, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(layout.createSequentialGroup()
+                                            .addComponent(JCBSucursal, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                            .addComponent(jLabel3)
+                                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                            .addComponent(JCBTipo, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addComponent(jLabel4)))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(221, 221, 221)
+                            .addComponent(JBEjecutaC2))))
+                .addContainerGap(47, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JCBSucursal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(JCBTipo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(15, 15, 15)
+                .addComponent(JBEjecutaC1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 150, Short.MAX_VALUE)
+                .addComponent(JBEjecutaC2)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22))
         );
 
         pack();
@@ -55,6 +240,64 @@ public class Reportes extends javax.swing.JFrame {
         vH.setVisible(true);
         this.dispose();           // TODO add your handling code here:
     }//GEN-LAST:event_formWindowClosing
+
+    private void JBEjecutaC1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBEjecutaC1ActionPerformed
+        // TODO add your handling code here:
+        modelo.setRowCount(0);
+        
+        String datos[] = new String[3];
+        
+        String idSucursal = ((String)JCBSucursal.getSelectedItem()).substring(0, 1);        
+                        
+        String tipo = (String)JCBTipo.getSelectedItem();
+        
+        if(tipo == "Arrendado")
+        {
+            modelo_tabla_Arrendado();
+            try {
+            Statement at = conexion.createStatement();
+            ResultSet rs = at.executeQuery("SELECT Paqueteria.Arrendado.tiempoContrato, COUNT(Paqueteria.Arrendado.tiempoContrato) FROM Paqueteria.Camion "
+                    + "INNER JOIN Paqueteria.Arrendado ON Paqueteria.Camion.idCamion = Paqueteria.Arrendado.idCamion " 
+                    + "WHERE Paqueteria.Camion.idSucursal = "+idSucursal+" GROUP BY(Paqueteria.Arrendado.tiempoContrato) ");
+            while(rs.next())
+            {
+                datos[0] = rs.getString("tiempoContrato");
+                datos[1] = rs.getString("count");
+
+                modelo.addRow(datos);
+            }           
+            rs.close();
+            at.close();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "No pudimos obtener los datos");
+            }
+        }
+        else{
+
+            modelo_tabla_Comprado();
+           try {
+            Statement at = conexion.createStatement();
+            ResultSet rs = at.executeQuery("SELECT COUNT(Paqueteria.comprado.fechacompra), EXTRACT (YEAR FROM Paqueteria.comprado.fechacompra) as cs\n" +
+            "FROM Paqueteria.Comprado INNER JOIN Paqueteria.camion ON Paqueteria.Comprado.idCamion = Paqueteria.camion.idCamion\n" +
+            "WHERE Paqueteria.camion.idSucursal = "+idSucursal+" GROUP BY (cs)");
+            while(rs.next())
+            {
+                datos[0] = rs.getString("count");
+                datos[1] = rs.getString("cs");
+
+                modelo.addRow(datos);
+            }           
+            rs.close();
+            at.close();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "No pudimos obtener los datos");
+            } 
+        }                
+    }//GEN-LAST:event_JBEjecutaC1ActionPerformed
+
+    private void JBEjecutaC2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBEjecutaC2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JBEjecutaC2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -92,5 +335,17 @@ public class Reportes extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton JBEjecutaC1;
+    private javax.swing.JButton JBEjecutaC2;
+    private javax.swing.JComboBox<String> JCBSucursal;
+    private javax.swing.JComboBox<String> JCBTipo;
+    private javax.swing.JTable JTCamiones;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
